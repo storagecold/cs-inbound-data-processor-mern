e396c43be992f9e368a9502d7bea453a43e1edff
rmohan17 <65498856+rmohan17@users.noreply.github.com>
2022-01-12 21:37:40 -0500

    added call back

--- src/service/validation.js
+++ src/service/validation.js
@@ -1,9 +1,37 @@
 const utils = require("./../util/utils");
-function validateFile(dataFile) {
-  let isColdExists = false;
-  let submitter = utils.getSubmitter(dataFile);
+const http = require("http");
+const URLCOLDDB = require("./../util/constants");
 
-  return isColdExists;
+function validateFile(dataFile) {
+  let isColdExist = false;
+  const submitter = utils.getSubmitter(dataFile);
+
+  const submitterDB = isColdExists(submitter);
+  if (submitterDB) {
+    isColdExist = true;
+  }
+  return isColdExist;
+}
+
+function isColdExists(submitter) {
+  const db = getDatabaseObject();
+  var dbo = db.db("coldDB");
+  dbo.collection("amad").findOne({}, function (err, result) {
+    if (err) throw err;
+    console.log(result.party);
+    db.close();
+  });
+}
+
+function getDatabaseObject() {
+  const mongoClient = require("mongodb").MongoClient;
+  const dbo = null;
+  mongoClient.connect(URLCOLDDB, (err, db) => {
+    if (err) throw err;
+    console.log("connected to database-----> ");
+    dbo = db;
+  });
+  return dbo;
 }
 
 module.exports = { validateFile };
---
+++ test.js
@@ -0,0 +1,12 @@
+const mongoClient = require("mongodb").MongoClient;
+
+mongoClient.connect("mongodb://localhost:27017/CS_DEV", (err, db) => {
+  if (err) throw err;
+  console.log("connected to database========");
+  var dbo = db.db("CS_DEV");
+  dbo.collection("coldInfo").findOne({}, function (err, result) {
+    if (err) throw err;
+    console.log(result.submitterId);
+    db.close();
+  });
+});
