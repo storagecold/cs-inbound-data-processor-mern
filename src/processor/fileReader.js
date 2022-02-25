782ef25b4e6a4dc2a4160edfa9f0eac781c062cd
rmohan17 <65498856+rmohan17@users.noreply.github.com>
2022-02-24 22:17:35 -0500

    added other tables

--- src/processor/fileReader.js
+++ src/processor/fileReader.js
@@ -14,11 +14,65 @@ async function readDataFile(dataFile) {
     const connection = ADODB.open(
       `Provider=Microsoft.Jet.OLEDB.4.0;Data Source=${config.INBOUND}${dataFile};Jet OLEDB:Database Password=state;Persist Security Info=False;`
     );
+    //loadGrp
+    await loadData(connection, dataFile);
     //load Amad
     await readAmad(connection, dataFile);
     //load Grp
   } catch (e) {
     loggger.error(e);
+  }
+}
+
+async function loadData(connection, dataFile) {
+  const grpTable = await connection.query(
+    "SELECT descrip,add1,under,open,dr,close,balance,Nature,openOTHER,AccName from grp"
+  );
+  for (let i = 0; i <= grpTable.length; i++) {
+    let grpRecord = grpTable[i];
+    if (!(grpRecord == undefined)) {
+      console.log("grpRecord ==> " + grpRecord);
+
+      //populate grp schema.
+      const amadData = await connection.query(
+        `SELECT AMADNO,DATE,PARTY,VILL,PKT3,COMM ,KISM ,MARK1 ,YR,Room ,chatta,gulla from amad where PARTY = ${grpRecord.descrip}`
+      );
+      for (let i = 0; i <= amadData.length; i++) {
+        let amadRecord = amadData[i];
+        if (!(amadRecord == undefined)) {
+          const amad = await Amad.create({
+            coldId: coldId,
+            amadNo: amadRecord.AMADNO,
+            entryDate: amadRecord.DATE,
+            party: amadRecord.PARTY,
+            village: amadRecord.VILL,
+            packets: amadRecord.PKT3,
+            commodity: amadRecord.COMM,
+            kism: amadRecord.KISM,
+            lotNo: amadRecord.MARK1,
+            year: amadRecord.DATE.substring(0, 4),
+            chamberNo: amadRecord.Room,
+            chatta: amadRecord.chatta,
+            gulla: amadRecord.gulla,
+          });
+          amad.save();
+          loggger.info(amad);
+        }
+      }
+      //load partyACC data.
+      const partyACC = await connection.query(
+        `select Code,BankCode,Branch,AccNo,IFSC,Chq1,Chq2,Chq3,BankName,AccName,Want2Print from Party where DESC = ${grpRecord.descrip}`
+      );
+
+      for (let i = 0; i <= partyACC.length; i++) {
+        let partyacc = partyACC[i];
+        if (!(partyacc == undefined)) {
+          console.log("partyacc ==> " + partyacc);
+        }
+      }
+
+      //same do here fro partylazer.
+    }
   }
 }
 
